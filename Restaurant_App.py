8041f8dc79682f2f56c44369577f340f626c1b99
nawalsinghfartiyal-creator <nawalsinghfartiyal@gmail.com>
2025-10-11 11:46:12 +0530

    Add files via upload

---
+++ Restaurant_App.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+from streamlit_jupyter import StreamlitPatcher
+StreamlitPatcher().jupyter()  
+
+
+# In[9]:
+
+
+import streamlit as st
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+
+# In[10]:
+
+
+st.set_page_config(layout = 'wide')
+
+
+# In[21]:
+
+
+scaler = StandardScaler()
+
+
+# In[22]:
+
+
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+joblib.dump(scaler, 'scaler.pkl')
+
+
+# In[12]:
+
+
+st.title("Restaurant Rating Prediction App")
+
+
+# In[23]:
+
+
+st.caption("This App helps to predict a Restaurant review class")
+
+
+# In[24]:
+
+
+st.divider()
+
+
+# In[25]:
+
+
+averagecost = st.number_input("Please enter the estimated average cost for two" , min_value = 50 , max_value = 99999 , value = 1000 , step = 200)
+
+
+# In[26]:
+
+
+tablebooking = st.selectbox("Restaurant has Table booking ?" , ["Yes" , "No"])
+
+
+# In[27]:
+
+
+onlinedelivery = st.selectbox("Restaurant has online delivery ?" , ["Yes" , "No"])
+
+
+# In[28]:
+
+
+pricerange = st.selectbox("What is the price range (1 Cheapest , 4 Most Expensie)" , [1, 2, 3, 4])
+
+
+# In[29]:
+
+
+predictbutton = st.button("Predict the Review!")
+
+
+# In[30]:
+
+
+st.divider()
+
+
+# In[31]:
+
+
+model = joblib.load("mlmodel.pkl")
+
+
+# In[32]:
+
+
+bookingstatus = 1 if tablebooking == 'Yes' else 0
+
+
+# In[33]:
+
+
+deliverystatus = 1 if onlinedelivery == 'Yes' else 0
+
+
+# In[34]:
+
+
+import numpy as np
+
+def flatten_scalar(x):
+    return np.ravel(x)  
+
+averagecost = flatten_scalar(averagecost)
+bookingstatus = flatten_scalar(bookingstatus)
+deliverystatus = flatten_scalar(deliverystatus)
+pricerange = flatten_scalar(pricerange)
+
+my_X_values = np.column_stack((averagecost, bookingstatus, deliverystatus, pricerange))
+
+print(my_X_values)
+print(my_X_values.shape)
+
+
+# In[35]:
+
+
+import joblib
+
+scaler = joblib.load("scaler.save")    
+X_new = scaler.transform(my_X_values)  
+
+
+# In[37]:
+
+
+if predictbutton:
+    st.snow()
+    prediction = model.predict(X_new)
+    if prediction < 2.5:
+        st.write("Poor")
+    elif prediction < 3.5:
+        st.write("Average")
+    elif prediction < 4.0:
+        st.write("Good")
+    elif prediction < 4.5:
+        st.write("Very Good")
+    else:
+        st.write("Excellent")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
